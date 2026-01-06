568863db0134a26e310c1c34626e3649f1167dbb
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2026-01-06 10:20:35 +0500

    fddfbdnfgnfgnf

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 // API manzilini shu yerdan o'zgartirsangiz, barcha sahifalarda o'zgaradi.
 
 // config.js faylida
-// export const API_BASE_URL = 'https://quizvds.up.railway.app';
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://quizvds.up.railway.app';
+// export const API_BASE_URL = 'http://localhost:8000';
